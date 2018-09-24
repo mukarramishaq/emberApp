5f30b34e67c93465884dbb0d2c32ef21c9718173
Mukarram Ishaq <mukarram.ishaq@rolustech.net>
2018-09-24 18:56:34 +0500

    created routes and models

--- app/components/view-one.js
+++ app/components/view-one.js
@@ -1,4 +1,8 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  "isH1" : false,
+  actions: {
+    "toggleProperty" : function(){this.toggleProperty("isH1")},
+  }
 });
